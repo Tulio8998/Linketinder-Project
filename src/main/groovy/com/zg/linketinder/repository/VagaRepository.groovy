80dfde69ad6d3369368013cf28162252de274f16
tulio <tulio89lopes@gmail.com>
2026-08-15 20:52:40 -0300

    feat: implementa metodos de adicionar e listar em repository

--- src/main/groovy/com/zg/linketinder/repository/VagaRepository.groovy
+++ src/main/groovy/com/zg/linketinder/repository/VagaRepository.groovy
@@ -1,4 +1,16 @@
 package com.zg.linketinder.repository
 
+import com.zg.linketinder.model.Empresa
+import com.zg.linketinder.model.Vaga
+
 class VagaRepository {
+    List<Vaga> vagas = []
+
+    def adicionarVaga(Vaga vaga) {
+        vagas.add(vaga)
+    }
+
+    def listarTodos() {
+        return vagas
+    }
 }
