5f49f7821c2da77abc16c62f836425f9812bc9a4
tulio <tulio89lopes@gmail.com>
2026-08-17 15:34:22 -0300

    reafactor: instancia objetos no repository

--- src/main/groovy/com/zg/linketinder/repository/CandidatoRepository.groovy
+++ src/main/groovy/com/zg/linketinder/repository/CandidatoRepository.groovy
@@ -5,6 +5,54 @@ import com.zg.linketinder.model.Candidato
 class CandidatoRepository {
     List<Candidato> candidatos = []
 
+    CandidatoRepository() {
+        candidatos.add(new Candidato(
+                cpf: "000.000.000-00",
+                idade: 21,
+                nome: "Túlio",
+                email: "teste@teste.com",
+                estado: "MG",
+                cep: "00000-000",
+                descricao: "Desenvolvedor",
+        ))
+        candidatos.add(new Candidato(
+                cpf: "111.111.111-11",
+                idade: 21,
+                nome: "Kratos",
+                email: "teste@teste.com",
+                estado: "MG",
+                cep: "11111-111",
+                descricao: "Desenvolvedor",
+        ))
+        candidatos.add(new Candidato(
+                cpf: "222.222.222-22",
+                idade: 21,
+                nome: "Geraldao",
+                email: "teste@teste.com",
+                estado: "MG",
+                cep: "22222-222",
+                descricao: "Desenvolvedor",
+        ))
+        candidatos.add(new Candidato(
+                cpf: "333.333.333-33",
+                idade: 21,
+                nome: "Cristiano",
+                email: "teste@teste.com",
+                estado: "MG",
+                cep: "33333-333",
+                descricao: "Desenvolvedor",
+        ))
+        candidatos.add(new Candidato(
+                cpf: "444.444.444-44",
+                idade: 21,
+                nome: "Ronaldo",
+                email: "teste@teste.com",
+                estado: "MG",
+                cep: "44444-444",
+                descricao: "Desenvolvedor",
+        ))
+    }
+
     def adicionarCandidato(Candidato candidato) {
         candidatos.add(candidato)
     }
--- src/main/groovy/com/zg/linketinder/repository/VagaRepository.groovy
+++ src/main/groovy/com/zg/linketinder/repository/VagaRepository.groovy
@@ -6,11 +6,60 @@ import com.zg.linketinder.model.Vaga
 class VagaRepository {
     List<Vaga> vagas = []
 
+    VagaRepository(List<Empresa> empresas) {
+        if (empresas.size() >= 5) {
+            vagas.add( new Vaga(
+                    nome: "Back",
+                    descricao: "Desenvolver",
+                    estado: "MG",
+                    cidade: "Belo Horizonte",
+                    competencias: ["Desenvolvimento agil", "DevOps", "Python", "Linux", "Java", "Groovy"],
+                    empresa: empresas.get(0)
+            ))
+
+            vagas.add( new Vaga(
+                    nome: "DevOps",
+                    descricao: "Desenvolver",
+                    estado: "MG",
+                    cidade: "Belo Horizonte",
+                    competencias: ["TypeScript", "DevOps", "Python", "Linux", "Java", "Groovy"],
+                    empresa: empresas.get(1)
+            ))
+
+            vagas.add( new Vaga(
+                    nome: "Fullstack",
+                    descricao: "Desenvolver",
+                    estado: "MG",
+                    cidade: "Belo Horizonte",
+                    competencias: ["JavaScript", "DevOps", "Python", "Linux", "Java", "Groovy"],
+                    empresa: empresas.get(2)
+            ))
+
+            vagas.add( new Vaga(
+                    nome: "Data Analytic",
+                    descricao: "Desenvolver",
+                    estado: "MG",
+                    cidade: "Belo Horizonte",
+                    competencias: ["JavaScript", "DevOps", "Python", "Linux", "Java", "Groovy"],
+                    empresa: empresas.get(3)
+            ))
+
+            vagas.add( new Vaga(
+                    nome: "Front",
+                    descricao: "Desenvolver",
+                    estado: "MG",
+                    cidade: "Belo Horizonte",
+                    competencias: ["JavaScript", "DevOps", "Python", "Linux", "Java", "Groovy"],
+                    empresa: empresas.get(4)
+            ))
+        }
+    }
+
     def adicionarVaga(Vaga vaga) {
         vagas.add(vaga)
     }
 
-    def listarTodos() {
+    def listarTodos(){
         return vagas
     }
 }
