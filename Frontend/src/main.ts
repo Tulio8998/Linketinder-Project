3da885a00a685e257ddf83360d850bb4b3e3592f
tulio <tulio89lopes@gmail.com>
2026-08-27 20:24:07 -0300

    feat(frontend): implementa side bar de dash de candidatos

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -2,11 +2,12 @@ import './styles/global.css'
 import './styles/signIn.css'
 import './styles/signUpCandidato.css'
 import './styles/signUpEmpresa.css'
+import './styles/candidatoDash.css'
 import { configSkills } from './components/cadastro'
 import { signIn } from './pages/signIn'
 import { signUpCandidato } from './pages/signUpCandidato'
 import { signUpEmpresa } from './pages/signUpEmpresa'
-
+import { candidatoDash } from './pages/candidatoDash'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
@@ -30,8 +31,14 @@ function signUpPageEmpresa(): void {
     app.innerHTML = signUpEmpresa()
 }
 
+function candidatoDashPage() {
+    app.innerHTML = candidatoDash()
+}
+
 // signUpPageEmpresa()
 // configSkills()
 
-signUpPageCandidato()
-configSkills()
+// signUpPageCandidato()
+// configSkills()
+
+candidatoDashPage()
---
+++ Frontend/src/pages/candidatoDash.ts
@@ -0,0 +1,56 @@
+export function candidatoDash(): string {
+    return `
+        <section class="candidatoDash-class">
+            <div class="candidato-profile">
+                <h2>Linketinder</h2>
+                <div class="profile">
+                    <div class="profile-content">
+                        <p class="name">Túilo Vilela Lopes</p>
+                        <div>
+                            <p>Minas Gerais</p>
+                            <p>-</p>
+                            <p>Brasil</p>
+                        </div>
+                    </div>
+                    <p class="line"></p>
+                    <div class="about">
+                        <p class="about-title">Sobre mim</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis officia itaque quas quia quaerat, facere vel nam ullam dolorem corporis fugit a facilis. Quo, vitae. Autem consequatur in accusantium dolores?</p>
+                    </div>
+                    <div class="skills">
+                        <p class="skill-title">Habilidades</p>
+                        <div>
+                            <p class="skill">Java</p>
+                            <p class="skill">Java</p>
+                            <p class="skill">Java</p>
+                            <p class="skill">Java</p>
+                            <p class="skill">Java</p>
+                        </div>
+                    </div>
+                    <div class="log-out">
+                        <p class="line"></p>
+                        <div>
+                            <button class="btn-out">Sair</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="dash-content">
+                <div class="top-bar">
+                    <h2>ALGO AQUI</h2>
+                </div>
+                <div class="match-cards">
+                    <div class="card">
+                        <div class="info-job">
+                            
+                        </div>
+                        <div class="choice">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    `
+}
